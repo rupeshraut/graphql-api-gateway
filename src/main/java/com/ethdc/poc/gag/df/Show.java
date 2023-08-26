8ed1111f0d9057f8d918f1b49b848cfdfbcf64fe
rpraut <rupeshrautt@gmail.com>
2023-08-26 16:51:12 +0530

    - build.gradle - added netflix dgs dependencies - schema.graphqls - added schema - Show.java - DGS model - ShowsDatafetcher.java - DSG component

---
+++ src/main/java/com/ethdc/poc/gag/df/Show.java
@@ -0,0 +1,19 @@
+package com.ethdc.poc.gag.df;
+
+public class Show {
+    private final String title;
+    private final Integer releaseYear;
+
+    public Show(String title, Integer releaseYear) {
+        this.title = title;
+        this.releaseYear = releaseYear;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Integer getReleaseYear() {
+        return releaseYear;
+    }
+}
