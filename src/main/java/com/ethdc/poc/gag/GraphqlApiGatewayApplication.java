6b1091ed59b9301c7bc31a5b2b6c9109bbaf5adf
rpraut <rupeshrautt@gmail.com>
2023-08-26 16:03:52 +0530

    baseline version

---
+++ src/main/java/com/ethdc/poc/gag/GraphqlApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.ethdc.poc.gag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraphqlApiGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ethdc/poc/gag/GraphqlApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ethdc.poc.gag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
